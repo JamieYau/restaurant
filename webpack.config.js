4ab61b94262ab3862a171f19f169522ef2a61d8a
JamieYau <jamie.yau2002@me.com>
2023-07-24 20:39:42 +0100

    Set up Webpack project structure and configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,45 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
   },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: "eval",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
 };
