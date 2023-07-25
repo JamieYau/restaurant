46643cf08d1e4bcdeba099133d9ab66a22d986d3
JamieYau <jamie.yau2002@me.com>
2023-07-25 21:22:58 +0100

    Fix logo.png

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -1,5 +1,9 @@
 // render the homepage
 
 const renderHomePage = () => {
+    const content = document.querySelector("#content");
+
+    // create header
+    const header = document.createElement("header");
     
 }
